a778ccec749fb8f56311788a0d727672c6560d66
victorialbrock <35825804+victorialbrock@users.noreply.github.com>
2018-03-05 05:52:27 -0800

    Create challenge2.js

---
+++ challenge2.js
@@ -0,0 +1,56 @@
+function setup() {
+  createCanvas(400, 400);
+  background(255);
+  colorMode(HSB);
+  noStroke();
+}
+
+var diameter = 10;
+
+function draw() {
+  if (mouseIsPressed) {
+    ellipse(mouseX, mouseY, diameter);
+  }
+}
+
+//function keyPressed() {
+function keyPressed(r) {
+  print(key);
+  if (key == 'R') {
+    fill(0, 100, 100);
+  } else if (key == 'G') {
+    fill(100, 70, 100);
+  } else if (key == 'B') {
+    fill(210, 100, 100);
+  } else if (key == 'T') {
+    fill(180, 100, 100);
+  } else if (key == 'Y') {
+    fill(60, 100, 100);
+  } else if (key == 'P') {
+    fill(310, 100, 100);
+  }
+
+  if (key == 1) {
+    diameter = 10;
+  } else if (key == 2) {
+    diameter = 20;
+  } else if (key == 3) {
+    diameter = 30;
+  }
+
+
+
+function setup() {
+  createCanvas(400, 400);
+  background(255);
+  colorMode(HSB);
+  noStroke();
+  
+var diameter = 10;
+
+function draw() {
+  if (keyIsPressed) {
+    ellipse(mouseX, mouseY, diameter);
+  }
+}
+}
